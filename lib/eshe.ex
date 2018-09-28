ed1cc6b0cfb2df25e82607363bbbddeff240b6e7
kobatako <ju.koba.itit.shar@gmail.com>
2018-09-28 19:31:02 +0900

    first commit

---
+++ lib/eshe.ex
@@ -0,0 +1,18 @@
+defmodule Eshe do
+  @moduledoc """
+  Documentation for Eshe.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eshe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eshe_test.exs
@@ -0,0 +1,8 @@
+defmodule EsheTest do
+  use ExUnit.Case
+  doctest Eshe
+
+  test "greets the world" do
+    assert Eshe.hello() == :world
+  end
+end
