13bdeff216893ee650adb4bfb00805a36535bd6d
kobatako <ju.koba.itit.shar@gmail.com>
2018-10-05 08:11:08 +0900

    routerのDSLの定義

--- lib/eshe/supervisor.ex
+++ lib/eshe/supervisor.ex
@@ -2,12 +2,19 @@ defmodule Eshe.Supervisor do
   import Supervisor.Spec
   use GenServer
 
-  def start_link() do
+  import Eshe.Router
 
+  static do
+    add dest_route: [192, 168, 20, 0], subnetmask: [255, 255, 255, 0], nexthop: [],  out_interface: :eth1
+  end
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   @impl true
   def init([]) do
+    IO.inspect "gen server init"
     {:ok, []}
   end
 end
